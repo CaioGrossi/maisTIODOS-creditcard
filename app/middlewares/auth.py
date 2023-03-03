059f9babfe66cf607287ae6cbdaed88aa441ea3f
Caio Grossi <caiogrossi6@gmail.com>
2023-03-03 10:38:39 -0300

    free /docs route

--- app/middlewares/auth.py
+++ app/middlewares/auth.py
@@ -6,6 +6,9 @@ from app.settings import settings
 
 class AuthMiddleware:
     async def __call__(self, request: Request, call_next):
+        if request.url.path == "/docs" or request.url.path == "/openapi.json":
+            return await call_next(request)
+    
         auth = request.headers.get("Authorization", None)
 
         if not auth:
